d347819b5721035d0b2c257b691e62e7a6933340
Samersetred <samersetredram@gmail.com>
2025-03-20 12:58:48 +0000

    changing allowed hosts

--- config/settings.py
+++ config/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '127.0.0.1,localhost,.herokuapp.com').split(',')
 
